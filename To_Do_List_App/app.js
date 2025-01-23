eb7257814b7fc13b0e91f19ef68acb3e5a942f0d
Selfmade20 <tebogo1686@gmail.com>
2025-01-23 14:29:08 +0200

    Added the add task function to the app

--- To_Do_List_App/app.js
+++ To_Do_List_App/app.js
@@ -6,9 +6,30 @@ let tasks = document.querySelector('.tasks');
 // add button disabled
 
 input.addEventListener('keyup', () => {
-    if (input.value.trim() !== 0) {
+    if (input.value.trim() != 0) {
         addBtn.classList.add('active');
     } else {
         addBtn.classList.remove('active');
+    }
+})
+
+
+// add new items to list 
+
+addBtn.addEventListener('click', () =>{
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div')
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p>${input.value}</p>
+        <div class="item-btn">
+            <i class="fa-solid fa-pen-to-square"><button>Edit</button></i>
+            <i class="fa-solid fa-xmark"><button>Remove</button></i>
+        </div>
+        `
+        tasks.appendChild(newItem);
+        input.value = ''
+    } else {
+        alert("Please enter a task")
     }
 })
